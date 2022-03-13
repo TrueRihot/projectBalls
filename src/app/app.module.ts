4bf5a5735cfe66e2da60140ed6ab8e46b035539c
KevinStaehler <kevin@startup-werk.de>
2022-03-13 15:55:47 +0100

    primife Help-Page (toggle mit 'H')

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThreejsSceneComponent } from './components/threejs-scene/threejs-scene.component';
+import { HelpComponent } from './components/ui/help/help.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThreejsSceneComponent,
+    HelpComponent,
   ],
   imports: [
     BrowserModule,
