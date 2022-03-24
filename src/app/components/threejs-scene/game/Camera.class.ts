def1883a1f999217dfe2118ee9ec6a597cae4ddf
Moritz Theil <moritz@startup-werk.de>
2022-03-24 12:56:17 +0100

    Kamera settings geclampt, dass man nicht aus den boundries gerät

--- src/app/components/threejs-scene/game/Camera.class.ts
+++ src/app/components/threejs-scene/game/Camera.class.ts
@@ -3,6 +3,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 import {Game, gameInstance} from './Game.class';
 import Sizes from "./utils/sizes";
+import Debug from "./utils/Debugger.class";
 
 export default class Camera {
   public camera: THREE.PerspectiveCamera;
@@ -10,13 +11,17 @@ export default class Camera {
   private sizes: Sizes;
   private canvas: HTMLCanvasElement;
   private scene: THREE.Scene;
+
   public controls: OrbitControls;
+  private debug: Debug;
+  private debugFolder : any;
 
   constructor() {
     this.game = gameInstance;
     this.sizes = this.game.sizes;
     this.canvas = this.game.canvas;
     this.scene = this.game.scene;
+    this.debug = this.game.debug;
 
     this.setInstance();
     this.setControls();
@@ -33,6 +38,16 @@ export default class Camera {
   {
       this.controls = new OrbitControls(this.camera as THREE.PerspectiveCamera, this.canvas);
       this.controls.enableDamping = true;
+      this.controls.maxDistance = 10;
+      this.controls.minDistance = 3.1;
+      this.controls.maxPolarAngle = 1.16;
+      this.controls.minPolarAngle = 0.73;
+      this.controls.target.set(0, 2, 0);
+      this.controls.enablePan = false;
+      this.controls.saveState();
+      if (this.debug.active){
+        this.addControlDebug();
+      }
   }
 
   resize()
@@ -44,6 +59,23 @@ export default class Camera {
   update()
   {
       this.controls.update();
+  }
+
+  addControlDebug() {
+    this.debugFolder = this.debug.ui.addFolder('Camera');
+    this.debugFolder.add(this.camera.position, 'x').listen();
+    this.debugFolder.add(this.camera.position, 'y').listen();
+    this.debugFolder.add(this.camera.position, 'z').listen();
+
+    const controlFolder = this.debugFolder.addFolder('Controls');
+    controlFolder.add(this.controls, 'enableDamping');
+    controlFolder.add(this.controls, 'enablePan');
+    controlFolder.add(this.controls, 'dampingFactor', 0, 1);
+    controlFolder.add(this.controls, 'maxDistance', 0, 10).step(0.1);
+    controlFolder.add(this.controls, 'minDistance', 0, 10).step(0.1);
+    controlFolder.add(this.controls, 'maxPolarAngle', 0, Math.PI / 2).step(0.01);
+    controlFolder.add(this.controls, 'minPolarAngle', 0, Math.PI / 2).step(0.01);
+    controlFolder.add(this.controls, 'reset');
   }
 
 }
