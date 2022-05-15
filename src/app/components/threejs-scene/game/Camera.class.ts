c0bf191bf0f6353e4d266d8dc710e25a06723007
Moritz Theil <moritz@startup-werk.de>
2022-05-16 00:16:48 +0200

    camera tweening

--- src/app/components/threejs-scene/game/Camera.class.ts
+++ src/app/components/threejs-scene/game/Camera.class.ts
@@ -4,6 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import {Game, gameInstance} from './Game.class';
 import Sizes from "./utils/sizes";
 import Debug from "./utils/Debugger.class";
+import gsap from "gsap";
+import {Vector3} from "three";
 
 export default class Camera {
   public camera: THREE.PerspectiveCamera;
@@ -16,8 +18,12 @@ export default class Camera {
   private state: 'global' | 'local';
   private debug: Debug;
   private debugFolder : any;
+  private target: Vector3 = new Vector3(0, 2, 0);
+  private intween: Boolean;
 
   private debugObj : any;
+  private isInit: boolean = false;
+
 
   constructor() {
     this.game = gameInstance;
@@ -26,14 +32,15 @@ export default class Camera {
     this.scene = this.game.scene;
     this.debug = this.game.debug;
     this.state = 'global';
+    this.intween = false;
 
     this.debugObj = {
       toggleStuff: this.switchState.bind(this),
-
     }
 
     this.setInstance();
     this.setControls();
+    this.isInit = true;
   }
 
   setInstance()
@@ -67,28 +74,60 @@ export default class Camera {
 
   update()
   {
-    if (this.state === 'local') {
-      const pos = this.game.world.ballsArray[0].mesh.position;
-      this.controls.target.set(pos.x, pos.y, pos.z);
+    if (!this.isInit) return;
+
+    if (!this.intween && this.state === 'local') {
+      const pos = this.game.world.ballsArray[0]?.mesh.position;
+      this.target = pos ? pos : new Vector3(0, 2, 0);
     }
-      this.controls.update();
+    this.controls.target.set(this.target.x, this.target.y, this.target.z);
+    this.controls.update();
   }
 
   switchState(state: 'global' | 'local' = undefined): void {
+    // those are the default values if the state isnt global
+    // This dummy values are used to create tweens for the camera
+    let tweenValues = {
+      maxDistance: 1.5,
+      minDistance: 1,
+    }
+    let targetTween = new Vector3(0, 2, 0);
+
+    // state setting logic if an inuptstate is given or not
     if (state === undefined) {
       this.state = this.state === 'global' ? 'local' : 'global';
     }else {
       this.state = state;
     }
 
+    // Setting the control values so the gloabal tween gets set to global values
     if (this.state === 'global') {
-      this.controls.reset();
-      this.controls.maxDistance = 10;
-      this.controls.minDistance = 3.1;
-      return;
+      tweenValues.maxDistance = 10;
+      tweenValues.minDistance = 3.1;
+      targetTween = new Vector3(0, 2, 0);
     }
-    this.controls.maxDistance = 1.5;
-    this.controls.minDistance = 1;
+    // If the state is local, we need to get the position of the ball to tween to it
+    if (this.state === 'local') {
+      targetTween = this.game.world.ballsArray[0].mesh.position;
+    }
+
+    if (this.intween) return;
+    // camera zoom animation
+    gsap.to(this.controls, {...tweenValues, duration: 0.5, ease: 'power2'});
+    // camera target animation
+    gsap.to(this.target,
+      {
+        x:targetTween.x,
+        y:targetTween.y,
+        z:targetTween.z,
+        duration: .5,
+        ease: 'power2',
+        onComplete: () => {
+          this.intween = false;
+          this.target= targetTween;
+        }
+      });
+    this.intween = true;
   }
 
   addControlDebug() {
